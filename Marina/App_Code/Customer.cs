80628e6c3eb34a6f93075920247d8b9d0b96a2b8
xaniz25 <shanicetalan@gmail.com>
2019-07-22 13:10:52 -0600

    added login link to lease slip page & added customer and lease classes

---
+++ Marina/App_Code/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Marina
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set;}
+
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
---
+++ Marina/App_Code/Lease.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Marina.App_Code
+{
+    public class Lease
+    {
+        public int ID { get; set; }
+
+        public int SlipID { get; set; }
+
+        public int CustomerID { get; set; }
+    }
+}
